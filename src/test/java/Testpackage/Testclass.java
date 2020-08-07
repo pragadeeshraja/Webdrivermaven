ffc20ea852b584d17bf23641a35768e3ec161e33
pragadeesh <pragadeeshraja11@gmail.com>
2020-08-07 12:31:57 +0530

    Initial Commit

---
+++ src/test/java/Testpackage/Testclass.java
@@ -0,0 +1,43 @@
+package Testpackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Testclass {
+	
+public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setup() {
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		
+	}
+	@Test
+	public void acionclass() {
+		driver.get("https://jqueryui.com/resources/demos/droppable/default.html");
+		driver.manage().window().maximize();
+	
+		WebElement drago=driver.findElement(By.xpath("//div[@id='draggable']"));
+		WebElement dropo=driver.findElement(By.xpath("//div[@id='droppable']"));
+		
+		Actions act=new Actions(driver);
+		//act.dragAndDrop(drago, dropo).perform();
+		act.dragAndDropBy(drago, 50, 50).perform();
+	}
+
+	@AfterSuite
+	public void closure() {
+		driver.quit();
+	}
+
+
+}
